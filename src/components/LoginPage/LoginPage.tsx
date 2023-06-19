8944aecf9841765aa6b373139e0f104954b0a1da
Vi <vikamatuchko772@gmail.com>
2023-06-19 17:23:58 +0300

    comments out

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -4,100 +4,6 @@ import { Formik, Field, Form } from "formik";
 import * as Yup from "yup";
 import "components/LoginPage/_login.scss";
 
-// const validate = (values: any) => {
-//   let errors: any = {};
-//   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/g;
-
-//   if (!values.email) {
-//     errors.email = "Email is required";
-//     // handleLogin();
-//   }
-//   if (!regex.test(values.email)) {
-//     errors.email = "Invalid Email";
-//   }
-
-//   return errors;
-// };
-
-// const LoginPage = () => {
-//   const [loginInput, setLoginInput] = useState("");
-
-//   const SignupSchema = Yup.object().shape({
-//     email: Yup.string().email("Invalid email").required("Required"),
-//   });
-//   const loginStore: any = localStorage.getItem("login");
-//   const submitBtn = document.querySelector(".loginPage__submit");
-//   const navigate = useNavigate();
-
-//   const handleInput = (event: any) => {
-//     setLoginInput(event.target.value);
-//   };
-
-//   const handleLogin = () => {
-//     // validate(loginStore, loginInput);
-//     if (!loginStore) {
-//       localStorage.setItem("login", loginInput);
-//       navigate("/dashboard");
-//     } else if (loginInput.length > 1) {
-//       if (loginInput === loginStore) {
-//         navigate("/dashboard");
-//       } else {
-//         localStorage.setItem("login", loginInput);
-//         navigate("/dashboard");
-//       }
-//     }
-//   };
-
-//   // if (loginStore) {
-//   //   navigate("/dashboard");
-//   // } else {
-//   return (
-//     <div className="loginPage">
-//       <div className="loginPage__box">
-//         <h1 className="loginPage__header">Login form</h1>
-//         <Formik
-//           initialValues={{ email: "" }}
-//           onSubmit={handleLogin}
-//           // validationSchema={SignupSchema}
-//           // validate={validate}
-//         >
-//           {/* {(formik) => {
-//             const { errors, touched } = formik;
-//             return ( */}
-//           <Form className="loginPage__form">
-//             <label className="loginPage__label" htmlFor="email">
-//               Email
-//             </label>
-//             <Field
-//               name="email"
-//               type="email"
-//               className="loginPage__input"
-//               value={loginInput}
-//               onChange={handleInput}
-//             />
-//             {/* {errors.email && touched.email ? (
-//                   <div>{errors.email}</div>
-//                 ) : null} */}
-//             <button
-//               type="submit"
-//               value="Submit"
-//               disabled={!loginInput}
-//               className="btn loginPage__submit"
-//             >
-//               Submit
-//             </button>
-//           </Form>
-//           {/* );
-//           }} */}
-//         </Formik>
-//       </div>
-//     </div>
-//   );
-//   // }
-// };
-
-// export default LoginPage;
-
 const DisplayingErrorMessagesSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email"),
 });
@@ -108,7 +14,6 @@ const validate = (values: any, func: any) => {
 
   if (!regex.test(values)) {
     errors.email = "Invalid Email";
-
     console.log(errors.email);
   } else {
     func();
