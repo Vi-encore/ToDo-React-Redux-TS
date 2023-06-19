eb8f8363be28929aa6f0c43c1ed2f7f99825550d
Vi <vikamatuchko772@gmail.com>
2023-06-20 01:59:00 +0300

    comments out

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,100 +1,9 @@
-import { useState } from "react";
+// import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import { object, string } from "yup";
 import routes from "components/utils/Routes";
 import "components/LoginPage/_login.scss";
-
-// const validate = (values: string, func: any, setValid: any) => {
-//   const errors: any = {};
-//   const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-//   if (!regex.test(values)) {
-//     errors.email = "Invalid Email";
-//     setValid(false);
-//     console.log(errors.email);
-//   } else {
-//     func();
-//   }
-
-//   return errors;
-// };
-
-// const LoginPage = () => {
-//   const [loginInput, setLoginInput] = useState("");
-//   const [valid, setValid] = useState(true);
-//   const loginStore: string | null = localStorage.getItem("login");
-//   const navigate = useNavigate();
-
-//   const formik = useFormik({
-//     initialValues: {
-//       email: "",
-//     },
-//     validationSchema: object({
-//       email: string().email("Invalid email address"),
-//     }),
-//     onSubmit: () => {
-//       validate(loginInput, handleLogin, setValid);
-//     },
-//   });
-
-//   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setLoginInput(e.target.value);
-//     setValid(true);
-//   };
-
-//   const handleLogin = () => {
-//     if (!loginStore) {
-//       localStorage.setItem("login", loginInput);
-//       navigate(routes.dashboard);
-//       setValid(false);
-//     } else if (loginInput.length > 1) {
-//       if (loginInput === loginStore) {
-//         navigate(routes.dashboard);
-//       } else {
-//         localStorage.setItem("login", loginInput);
-//         navigate(routes.dashboard);
-//       }
-//     }
-//   };
-
-//   return (
-//     <div className="loginPage">
-//       <div className="loginPage__box">
-//         <h1 className="loginPage__header">Login form</h1>
-
-//         <form className="loginPage__form" onSubmit={formik.handleSubmit}>
-//           <label htmlFor="email" className="loginPage__label">
-//             Email
-//           </label>
-//           <input
-//             name="email"
-//             type={"text"}
-//             // onChange={formik.handleChange}
-//             onBlur={formik.handleBlur}
-//             // value={formik.values.email && loginInput}
-//             //helperText={formik.errors.email ? formik.errors.email : ""}
-//             className={valid ? "loginPage__input" : "loginPage__input--error"}
-//             value={loginInput}
-//             onChange={handleInput}
-//           />
-//           {formik.touched.email && formik.errors.email && (
-//             <div>{formik.errors.email}</div>
-//           )}
-
-//           <button
-//             type="submit"
-//             value="Submit"
-//             disabled={!loginInput}
-//             className="btn loginPage__submit"
-//           >
-//             Submit
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
 
 const LoginPage = () => {
   // const [loginInput, setLoginInput] = useState("");
@@ -130,11 +39,6 @@ const LoginPage = () => {
       // console.log(formik.dirty);
     },
   });
-
-  // const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setLoginInput(e.target.value);
-  //   setValid(true);
-  // };
 
   const handleLogin = () => {
     //   if (!formik.values.email) {
