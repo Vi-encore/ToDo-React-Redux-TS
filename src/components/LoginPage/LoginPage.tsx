b37ef666d14fbf29b88e713f890a43d707c09dd2
Vi <vikamatuchko772@gmail.com>
2023-06-19 18:25:50 +0300

    useFormik

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,34 +1,36 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Formik, Field, Form } from "formik";
-import { object, string } from "yup";
+import { useFormik } from "formik";
+// import { object, string } from "yup";
 import "components/LoginPage/_login.scss";
-
-const DisplayingErrorMessagesSchema = object().shape({
-  email: string().email("Invalid email"),
-});
-
-const validate = (values: any, func: any) => {
-  const errors: any = {};
-  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-  if (!regex.test(values)) {
-    errors.email = "Invalid Email";
-    console.log(errors.email);
-  } else {
-    func();
-  }
-
-  return errors;
-};
 
 const LoginPage = () => {
   const [loginInput, setLoginInput] = useState("");
   const loginStore: any = localStorage.getItem("login");
   const navigate = useNavigate();
-  // const submitBtn: any = document.querySelector(".loginPage__submit");
-  // const inputField: any = document.querySelector(".loginPage__input");
-  // const navigate = useNavigate();
+
+  const validate = (values: any, func: any) => {
+    const errors: any = {};
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!regex.test(values)) {
+      errors.email = "Invalid Email";
+      console.log(errors.email);
+    } else {
+      func();
+    }
+
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    onSubmit: () => {
+      validate(loginInput, handleLogin);
+    },
+  });
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setLoginInput(e.target.value);
@@ -52,38 +54,28 @@ const LoginPage = () => {
     <div className="loginPage">
       <div className="loginPage__box">
         <h1 className="loginPage__header">Login form</h1>
-        <Formik
-          initialValues={{
-            email: "",
-          }}
-          validationSchema={DisplayingErrorMessagesSchema}
-          onSubmit={() => {
-            validate(loginInput, handleLogin);
-          }}
-        >
-          {/* {({ errors, touched }) => ( */}
-          <Form className="loginPage__form">
-            <Field
-              name="email"
-              type="email"
-              className="loginPage__input"
-              value={loginInput}
-              onChange={handleInput}
-            />
-            {/* {/* If this field has been touched, and it contains an error, display
-          it */}
-            {/* {touched.email && errors.email && <div>{errors.email}</div>} */}
-            <button
-              type="submit"
-              value="Submit"
-              disabled={!loginInput}
-              className="btn loginPage__submit"
-            >
-              Submit
-            </button>
-          </Form>
-          {/* )} */}
-        </Formik>
+
+        <form className="loginPage__form" onSubmit={formik.handleSubmit}>
+          <label htmlFor="email" className="loginPage__label">
+            Email
+          </label>
+          <input
+            name="email"
+            type="email"
+            className="loginPage__input"
+            value={loginInput}
+            onChange={handleInput}
+          />
+
+          <button
+            type="submit"
+            value="Submit"
+            disabled={!loginInput}
+            className="btn loginPage__submit"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
