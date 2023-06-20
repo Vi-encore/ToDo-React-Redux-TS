0fca800f26aade752a0429009abe4c56e3014ad0
Vi <vikamatuchko772@gmail.com>
2023-06-20 15:09:02 +0300

    box-sizing => border-box

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import routes from "components/utils/routes";
+import "components/Dashboard/_dashboard.scss";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -16,45 +17,49 @@ const Dashboard = () => {
           Log out
         </button>
       </div>
-      <button
-        className="dashboard__create-card-btn"
-        onClick={() => {
-          console.log("Create Card btn");
-        }}
-      >
-        Create Card
-      </button>
-      <div className="dashboard__cards">
-        <div className="dashboard__card">
-          <div className="dashboard__card-textarea">
-            <h2 className="dashboard__card-textarea--title">Card title</h2>
-            <p className="dashboard__card-textarea--text">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-          </div>
-          <div className="dashboard__card-btns">
-            <button
-              className="dashboard__card-btns--edit"
-              onClick={() => {
-                console.log("Edit btn");
-              }}
-            >
-              Edit
-            </button>
-            <button
-              className="dashboard__card-btns--delete"
-              onClick={() => {
-                console.log("Delete btn");
-              }}
-            >
-              Delete
-            </button>
+      <div className="dashboard__wrap">
+        <div className="dashboard__create-card-btn--wrap">
+          <button
+            className="dashboard__create-card-btn"
+            onClick={() => {
+              console.log("Create Card btn");
+            }}
+          >
+            Create Card
+          </button>
+        </div>
+        <div className="dashboard__cards">
+          <div className="dashboard__card">
+            <div className="dashboard__card-textarea">
+              <h2 className="dashboard__card-textarea--title">Card title</h2>
+              <p className="dashboard__card-textarea--text">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+            </div>
+            <div className="dashboard__card-btns">
+              <button
+                className="dashboard__card-btns--edit"
+                onClick={() => {
+                  console.log("Edit btn");
+                }}
+              >
+                Edit
+              </button>
+              <button
+                className="dashboard__card-btns--delete"
+                onClick={() => {
+                  console.log("Delete btn");
+                }}
+              >
+                Delete
+              </button>
+            </div>
           </div>
         </div>
       </div>
