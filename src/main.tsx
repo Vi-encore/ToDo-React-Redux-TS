47ea2444999496e8a0b4fdf748a1ac6b6ef6ae10
Vi <vikamatuchko772@gmail.com>
2023-06-21 12:41:29 +0300

    nc

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "src/App";
-// import "src/styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
