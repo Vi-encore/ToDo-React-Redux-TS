8f8f6240d6a2f7cd8b96fa89b18bc146313a1a0b
Vi <vikamatuchko772@gmail.com>
2023-06-23 16:33:10 +0300

    _dashboard mov vers style added

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -34,7 +34,83 @@ const Dashboard = () => {
             Create Card
           </button>
         </div>
+        {/* <div className="dashboard__cards--wrap">  */}
         <div className="dashboard__cards">
+          <div className="dashboard__cards-card">
+            <div className="dashboard__cards-card-textarea">
+              <h2 className="dashboard__cards-card-textarea--title">
+                Card title
+              </h2>
+              <div className="dashboard__cards-card-textarea--wrap">
+                <p className="dashboard__cards-card-textarea--text">
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum."
+                </p>
+              </div>
+            </div>
+            <div className="dashboard__cards-card-btns">
+              <button
+                className=" dashboardCardBtn dashboard__cards-card-btns--edit"
+                onClick={() => {
+                  console.log("Edit btn");
+                }}
+              >
+                Edit
+              </button>
+              <button
+                className="dashboardCardBtn dashboard__cards-card-btns--delete "
+                onClick={() => {
+                  console.log("Delete btn");
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+          {/* </div> */}
+          <div className="dashboard__cards-card">
+            <div className="dashboard__cards-card-textarea">
+              <h2 className="dashboard__cards-card-textarea--title">
+                Card title
+              </h2>
+              <div className="dashboard__cards-card-textarea--wrap">
+                <p className="dashboard__cards-card-textarea--text">
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum."
+                </p>
+              </div>
+            </div>
+            <div className="dashboard__cards-card-btns">
+              <button
+                className=" dashboardCardBtn dashboard__cards-card-btns--edit"
+                onClick={() => {
+                  console.log("Edit btn");
+                }}
+              >
+                Edit
+              </button>
+              <button
+                className="dashboardCardBtn dashboard__cards-card-btns--delete "
+                onClick={() => {
+                  console.log("Delete btn");
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
           <div className="dashboard__cards-card">
             <div className="dashboard__cards-card-textarea">
               <h2 className="dashboard__cards-card-textarea--title">
