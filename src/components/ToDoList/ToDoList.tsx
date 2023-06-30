0cd700b3d76eb10c07065a3ab4d88668137708ed
Vi <vikamatuchko772@gmail.com>
2023-06-30 13:45:30 +0300

    basic structure of get

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,27 +1,14 @@
-// import fetchList from "app/fetches/fetchList";
 // import { isFulfilled } from "@reduxjs/toolkit";
 
 import Card from "components/Card/Card";
 
-const ToDoList = () => {
-  // const result = fetchList("dasf");
-  // // console.log(result.then(Pr));
-  // const fulfilled = result.then(isFulfilled);
-
-  // if (!fulfilled) {
-  //   const fulfilled = result.then(isFulfilled);
-  //   return (
-  //     <>
-  //       <div>LOADING</div>
-  //     </>
-  //   );
-  // }
+const ToDoList = (resp: any) => {
+  //map through resp => give info to Card
 
   return (
     <div className="dashboard__cards">
       <Card>Card title</Card>
       <Card>Card title</Card>
-      {/* </div> */}
     </div>
   );
 };
