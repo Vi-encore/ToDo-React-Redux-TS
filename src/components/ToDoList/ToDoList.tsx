78a62eb5bfcc93c7f9baec342df1c85af65a4cdf
Vi <vikamatuchko772@gmail.com>
2023-06-30 19:55:40 +0300

    nc

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -10,45 +10,39 @@
 //   allToDosReceived,
 // } from "slices/todoSlices";
 import Card from "components/Card/Card";
-import getInitToDos from "fetches/getInitToDos";
+// import getInitToDos from "fetches/getInitToDos";
 
 const ToDoList = (resp: any, email: string) => {
-  // const test = [
-  //   {
-  //     id: 1,
-  //     title: "here is title1",
-  //     description: "i am description1",
-  //     user: "email",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "here is title2",
-  //     description: "i am description2",
-  //     user: "email",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "here is title3",
-  //     description: "i am description3",
-  //     user: "email",
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "here is title4",
-  //     description: "i am description4",
-  //     user: "email",
-  //   },
-  // ];
+  const test = [
+    {
+      id: 1,
+      title: "here is title1",
+      description: "i am description1",
+      user: "email",
+    },
+    {
+      id: 2,
+      title: "here is title2",
+      description: "i am description2",
+      user: "email",
+    },
+    {
+      id: 3,
+      title: "here is title3",
+      description: "i am description3",
+      user: "email",
+    },
+    {
+      id: 4,
+      title: "here is title4",
+      description: "i am description4",
+      user: "email",
+    },
+  ];
   //map through resp => give info to Card
-  // if(resp.length === 0) {
-  // return <div className="dashboard__cards">No items found</div>
-  // } else {
-  // const list = () => resp.map((item: any) => Card(item))
-  // return (<div className="dashboard__cards">{list}</div>)
-  //  }
 
   if (Object.keys(resp).length === 0) {
-    return <div className="dashboard__cards">No items found</div>;
+    return <div className="dashboard__cards">No Items Found</div>;
   } else {
     const list = Object.keys(resp).map((item: any) => Card(item));
     return <div className="dashboard__cards">{list}</div>;
