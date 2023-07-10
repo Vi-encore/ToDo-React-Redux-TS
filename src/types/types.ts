0b0bb81c55e7a86533c5bbabcb9e72303f9475cd
Vi <vikamatuchko772@gmail.com>
2023-07-10 18:22:40 +0300

    nc

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,31 +1,3 @@
-// export type INote = {
-//   id: string;
-//   title: string;
-//   description: string;
-//   createdAt: Date;
-//   updatedAt: Date;
-// };
-// export type IMutateNote = {
-//   title: string;
-//   description: string;
-// };
-
-// export type IGenericResponse = {
-//   status: string;
-//   message: string;
-// };
-
-// export type INoteResponse = {
-//   status: string;
-//   note: INote;
-// };
-
-// export type INotesResponse = {
-//   status: string;
-//   results: number;
-//   notes: INote[];
-// };
-
 export type createModalType = {
   children: string;
   modalOpen: boolean;
